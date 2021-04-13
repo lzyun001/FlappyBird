28cc97d6267ce2b77625d35ac5cdaeacdaa2085b
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2021-04-13 20:40:03 +0800

    Done!

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public Vector2 position;
+    public float jumpHeight;
+    public int Score;
+    public bool isStart;
+    public bool isDead;
+
+    public Player(float jumpHeight)
+    {
+        this.jumpHeight = jumpHeight;
+    }
+}
+
+public class PlayerControl : MonoBehaviour
+{
+    public UIManager uiManager;
+    public Player Instance;
+    public AudioSource Audio_PassBlock;
+    public Animator ani;
+    private Rigidbody2D rb;
+    private Vector2 jumpForce;
+    private Vector2 playerInitPosition;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        Init();
+        playerInitPosition = gameObject.transform.position;
+        jumpForce = new Vector2(0, Instance.jumpHeight);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Init()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        Instance = new Player(300);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !Instance.isStart)
+        {
+            GameStart();
+        }
+
+        if (Instance.isStart && !Instance.isDead)
+        {
+            gameObject.transform.Translate(new Vector2(0.1f, 0));
+            if (Input.anyKeyDown)
+            {
+                rb.AddForce(jumpForce);
+                ani.SetTrigger("Jump");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            PlayerScoreAdd(1);
+            Audio_PassBlock.Play();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D obstacle)
+    {
+        if (obstacle.gameObject.tag == "Obstacle")
+        {
+            Dead();
+        }
+    }
+
+    private void GameStart()
+    {
+        uiManager.StopAllCoroutines();
+        uiManager.Text_Start.enabled = false;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        Instance.isStart = true;
+    }
+
+    public void PlayerScoreAdd(int score)
+    {
+        Instance.Score += score;
+        uiManager.Text_PlayerScore.text = "Score: " + Instance.Score;
+    }
+
+    public void Dead()
+    {
+        ShowScoreBoard();
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        Instance.isDead = true;
+    }
+
+    private void ShowScoreBoard()
+    {
+        uiManager.ScoreBoard.SetActive(true);
+        uiManager.Text_ScoreEnd.text = "Your Score is :" + Instance.Score;
+    }
+
+    public void Restart()
+    {
+        Init();
+        gameObject.transform.position = playerInitPosition;
+        uiManager.Text_PlayerScore.text = "Score: " + Instance.Score;
+    }
+}
---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject Player;
+    public Text Text_Start;
+    public Text Text_PlayerScore;
+    public Text Text_ScoreEnd;
+    public Button Btn_Quit;
+    public Button Btn_Restart;
+    public GameObject ScoreBoard;
+    public GameManager gameManager;
+    private IEnumerator coroutine;
+
+    private void Awake()
+    {
+        Btn_Quit.onClick.AddListener(BtnClick_Quit);
+        Btn_Restart.onClick.AddListener(BtnClick_Restart);
+        Init();
+    }
+
+    private void Init()
+    {
+        Text_Start.enabled = true;
+        coroutine = TextFade(0.75f);
+        StartCoroutine(coroutine);
+    }
+    
+    public IEnumerator TextFade(float fadeTimer)
+    {
+        while (true)
+        {
+            Text_Start.DOFade(150f / 255f, fadeTimer);
+            yield return new WaitForSeconds(fadeTimer);
+            Text_Start.DOFade(255 / 255, fadeTimer);
+            yield return new WaitForSeconds(fadeTimer);
+        }
+    }
+
+    private void BtnClick_Quit()
+    {
+        Application.Quit();
+    }
+
+    private void BtnClick_Restart()
+    {
+        Init();
+        ScoreBoard.SetActive(false);
+        Player.GetComponent<PlayerControl>().Restart();
+        gameManager.DestroyAllBlocks();
+    }
+}
